1f42814df04585f392b3f209a0bf08ff45e230e8
brials <brials008@hotmail.com>
2017-04-04 16:16:18 -0700

    completed restaraunt model.js file need to re adapt table router to fill in change with tables so they have an restaurantId

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Believe I will be eliminating Party as a model but rather semi replacing it with
 customer no longer has partyId to match removal of party as model. Also no more reservations array for a single customer rather just one reservation Id associated not required.
 
 table on reservation is now tableId;
+
+restaurant array of customers is now an array of tables.
--- model/restaurant.js
+++ model/restaurant.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const debug = require('debug')('restServ:restaurant');
+const createError = require('http-errors');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -9,7 +10,7 @@ const restaurantSchema = Schema({
   storeHours: {type: String, required: true},
   location: {type: String, required: true},
   employees: [{type: Schema.Types.ObjectId, ref: 'employee'}],
-  customers: [{type: Schema.Types.ObjectId, ref: 'customers'}],
+  tables: [{type: Schema.Types.ObjectId, ref: 'table'}],
   menuitems: [{type: Schema.Types.ObjectId, ref: 'menuitem'}]
 });
 
@@ -18,9 +19,83 @@ const Restaurant = module.exports = mongoose.mode('restaurant', restaurantSchema
 Restaurant.findByIdAndAddEmployee = function(id, employeeId){
   debug('Restaurant.findByIdAndAddEmployee');
 
-  Restaurant.findById(id)
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
   .then(restaurant => {
     restaurant.employees.push(employeeId);
-  });
-  //TODO finish this route after commit
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
+};
+
+Restaurant.findByIdAndRemoveEmployee = function(id, employeeId){
+  debug('Restaurant.findByIdAndRemoveEmployee');
+
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(restaurant => {
+    for(var i = 0; i < restaurant.employees.length; i++){
+      if(restaurant.employees[i].toString() == employeeId.toString()){
+        restaurant.employees.splice(i, 1);
+      }
+    }
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
+};
+
+Restaurant.findByIdAndAddTable = function(id, tableId){
+  debug('Restaurant.findByIdAndAddTable');
+
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(restaurant => {
+    restaurant.tables.push(tableId);
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
+};
+
+Restaurant.findByIdAndRemoveTable = function(id, tableId){
+  debug('Restaurant.findByIdAndRemoveTable');
+
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(restaurant => {
+    for(var i = 0; i < restaurant.tables.length; i++){
+      if(restaurant.tables[i].toString() == tableId.toString()){
+        restaurant.tables.splice(i, 1);
+      }
+    }
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
+};
+
+Restaurant.findByIdAndAddMenuitem = function(id, menuitemId){
+  debug('Restaurant.findByIdAndAddMenuitem');
+
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(restaurant => {
+    restaurant.menuitems.push(menuitemId);
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
+};
+
+Restaurant.findByIdAndRemoveMenuitem = function(id, menuitemId){
+  debug('Restaurant.findByIdAndRemoveMenuitem');
+
+  return Restaurant.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(restaurant => {
+    for(var i = 0; i < restaurant.menuitems.length; i++){
+      if(restaurant.menuitems[i].toString() == menuitemId.toString()){
+        restaurant.menuitems.splice(i, 1);
+      }
+    }
+    return Restaurant.findByIdAndUpdate(id, restaurant, {new: true});
+  })
+  .then(restaurant => restaurant);
 };
